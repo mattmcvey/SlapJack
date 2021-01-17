0d082b83c42c5b47e4e877a6312190fc06967ea3
Matt McVey <matt.mcvey49@gmail.com>
2021-01-17 11:32:48 -0700

    Add functionality to slap with sandwiches and doubles

--- game.js
+++ game.js
@@ -96,6 +96,9 @@ class Game {
     var previousCard = this.deck[1];
     var previousCardDashSplit = previousCard.split("-");
     var previousCardPeroidSplit = previousCardDashSplit[1].split(".")
+    var thirdCard = this.deck[2];
+    var thirdCardDashSplit = thirdCard.split("-");
+    var thirdCardPeriodSplit = thirdCardDashSplit[1].split(".")
     if(currentCardDashSplit[1] === "jack.png" && event.key === "f"){
       for(var i = 0; i < this.deck.length; i++){
         this.player1.hand.push(this.deck[i])
@@ -110,23 +113,35 @@ class Game {
       this.deck = [];
       this.deckType = "player2Deck";
       this.shuffle(this.player2.hand, this.player2.hand.length)
+    } else if(previousCardPeroidSplit[0] === currentCardPeriodSplit[0] && event.key === "f" && this.player1.hand.length && this.player2.hand.length) {
+      for(var i = 0; i < this.deck.length; i++){
+        this.player1.hand.push(this.deck[i])
+      }
+      this.deck = [];
+      this.deckType = "player1Deck";
+      this.shuffle(this.player1.hand, this.player1.hand.length)
+    } else if(previousCardPeroidSplit[0] === currentCardPeriodSplit[0] && event.key === "j" && this.player1.hand.length && this.player2.hand.length){
+      for(var i = 0; i < this.deck.length; i++){
+        this.player2.hand.push(this.deck[i])
+      }
+      this.deck = [];
+      this.deckType = "player2Deck";
+      this.shuffle(this.player2.hand, this.player2.hand.length)
+    } else if (currentCardPeriodSplit[0] === thirdCardPeriodSplit[0] && event.key === "f" && this.player1.hand.length && this.player2.hand.length){
+      for(var i = 0; i < this.deck.length; i++){
+        this.player1.hand.push(this.deck[i])
+      }
+      this.deck = [];
+      this.deckType = "player1Deck";
+      this.shuffle(this.player1.hand, this.player1.hand.length)
+    } else if (currentCardPeriodSplit[0] === thirdCardPeriodSplit[0] && event.key === "j" && this.player1.hand.length && this.player2.hand.length) {
+      for(var i = 0; i < this.deck.length; i++){
+        this.player2.hand.push(this.deck[i])
+      }
+      this.deck = [];
+      this.deckType = "player2Deck";
+      this.shuffle(this.player2.hand, this.player2.hand.length)
     }
-
-    // else if(previousCardPeroidSplit[0] === currentCardPeriodSplit[0] && event.key === "f" && this.player1.hand.length && this.player2.hand.length) {
-    //   for(var i = 0; i < this.deck.length; i++){
-    //     this.player1.hand.push(this.deck[i])
-    //   }
-    //   this.deck = [];
-    //   this.deckType = "player1Deck";
-    //   this.shuffle(this.player1.hand, this.player1.hand.length)
-    // } else if(previousCardPeroidSplit[0] === currentCardPeriodSplit[0] && event.key === "j" && this.player1.hand.length && this.player2.hand.length){
-    //   for(var i = 0; i < this.deck.length; i++){
-    //     this.player2.hand.push(this.deck[i])
-    //   }
-    //   this.deck = [];
-    //   this.deckType = "player2Deck";
-    //   this.shuffle(this.player2.hand, this.player2.hand.length)
-    // }
   }
   winCount() {
 
